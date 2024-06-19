d940c4175abf55df23ee591438e45bb47f979667
bhanusharanya <sharanya2630@gmail.com>
2024-06-19 19:16:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function toCase(text) {
   // write your code here
+ if (input === '') {
+        return '-';
+    }
+    
+    // Convert the input string to lowercase and uppercase
+    const lowerCase = input.toLowerCase();
+    const upperCase = input.toUpperCase();
+    
+    // Return the concatenated result with '-' as the delimiter
+    return `${lowerCase}-${upperCase}`;
 }
 
 // DO not change the code below
